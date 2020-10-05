aa44cdcd122657ab581223172356c68ee3be558e
Guilherme <guilhermesbw2@gmail.com>
2020-10-05 10:06:33 -0300

    fixed import bug

--- src/pages/Import/index.tsx
+++ src/pages/Import/index.tsx
@@ -25,7 +25,7 @@ const Import: React.FC = () => {
   async function handleUpload(): Promise<void> {
     const data = new FormData();
 
-    if (!uploadedFiles.length) return;
+    if (!uploadedFiles.length) return; // if files dont exist
 
     const file = uploadedFiles[0];
 
